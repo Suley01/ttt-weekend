337275a1d0cae4c89a6b2fd0b7243c8ed04ae7f4
Suleyma <suleymapizana@gmail.com>
2022-09-03 21:32:46 -0500

    Set board variable to array of nulls

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ let board, turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
-const squareEls = document.querySelectorAll("#square")
+const squareEls = document.querySelectorAll(".square")
 const messageEl = document.querySelector("#message")
 
 
@@ -16,36 +16,24 @@ const messageEl = document.querySelector("#message")
 
 
 /*-------------------------------- Functions --------------------------------*/
+init () 
+//calling function when app loads
 
+function init(){
+  board = [null, null, null, null, ,null, null, null, null, null]
+  //null meaning it is empty or unknown
 
-// Step 1 - Define the required variables used to track the state of the game 
-
-  //// 1a) Use a variable named `board` to represent the state of the squares on
-  // //   the board. 
-
-  //// 1b) Use a variable named `turn` to track whose turn it is.
-
-  //// 1c) Use a variable named `winner` to represent if anyone has won yet, or 
-  // //   if a tie has occurred.
-
-// Step 2 - Store cached element references
-
- // // 2a) In a constant called `squareEls`, store the nine elements 
- // //    representing the squares on the page.
-
-  // 2b) In a constant called `messageEl`, store the element that displays the 
-  //    game's status on the page.
-
+}
 
 // Step 3 - Upon loading, the game state should be initialized, and a function 
 //          should be called to render this game state
 
-  // 3a) Create a function called `init`.
+ // // 3a) Create a function called `init`.
 
-  // 3b) Call this `init` function when the app loads.
+  //// 3b) Call this `init` function when the app loads.
 
-  // 3c) Set the `board` variable to an array containing nine `null`s to 
-  //    represent empty squares.
+//  // 3c) Set the `board` variable to an array containing nine `null`s to 
+//  //    represent empty squares.
 
   // 3d) Set the `turn` to `1` - which will represent player X.
 
